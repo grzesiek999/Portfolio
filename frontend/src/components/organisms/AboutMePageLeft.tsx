552bef3c5766d2c9887952cce76db26b52f8cc5e
Grzegorz Pasich <grzegorz.pasich@o2.pl>
2024-10-19 16:25:09 +0200

    about me content

--- frontend/src/components/organisms/AboutMePageLeft.tsx
+++ frontend/src/components/organisms/AboutMePageLeft.tsx
@@ -3,6 +3,12 @@
 const AboutMePageLeft = () => {
     return(
         <div>
+            My name is Grzesiek Pasich, I live in small town Wolbrom which is located in south part of Poland next to Cracow.
+
+            I'm interested in computer science and electronics that's why i decided go to studies in this way.
+
+            I graduated engineering studies from computer science at the Cracow University of Technology and I'm continuate learning on second step studies with cybersecurity speciality.
+
             
         </div>
     )
